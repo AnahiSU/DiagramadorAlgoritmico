df0d95229d047650c1a8870dbece775fc266f589
Anahi <anahi7776@gmail.com>
2025-10-18 16:18:40 -0400

    Fix formatting by adding a newline at the end of script.js

--- script.js
+++ script.js
@@ -2,3 +2,4 @@ import { crearDecision } from './js/decisiones.js';
 import { crearProceso } from './js/procesos.js';
 import { crearSalida } from './js/salida.js';
 import * as interaccion from './js/interaccion.js';
+
